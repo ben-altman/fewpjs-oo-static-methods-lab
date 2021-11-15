25a242a8fff3e40774aea806b4434df0e8b602f2
github username <github email address>
2021-11-15 14:03:24 -0500

    commit tree f8384252d12a40206d54d40ef4292c6da03978cc

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    debugger
+    let uncap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let allWords = string.split(' ')
+
+    let mappedWords = allWords.map (word => {
+      if (!uncap.includes(word)) {
+      return this.capitalize(word);
+      } else {
+        return word;
+      }
+    })
+
+    // console.log(mappedWords.join(' '));
+    return this.capitalize(mappedWords.join(' '));
+  }
 }
